39e0f6abb99812e587e1494a871fea9891a09db0
Mrunali2310 <118894621+Mrunali2310@users.noreply.github.com>
2023-06-05 14:52:08 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'task';
+  UserData = [
+    {Name:'Mistletoey',song:'Justin Bieber',Class:'3:54',rating:'Female',image:'assets/recently played song image (1).png',image1:'',image2:''},
+    
+    
+    
+    
+  ]
+  
+}
+
+
